fc7acb92f4a75a7dab25b7cfb718ff3fdaee37cb
rlongdragon <jz744335@gamil.com>
2024-10-10 19:44:16 +0800

    week3-2

--- week3/2. 檢查簡單數列/main.c
+++ week3/2. 檢查簡單數列/main.c
@@ -1,27 +1,82 @@
 #include <stdio.h>
 
 int main() {
-  int m, n;
+  int m, n, num;
+  int isValid, isEnd;
   char c;
-  int scanfReturn;
   int i;
-  while (1) {
-    scanfReturn = scanf("%d%c", &m, &c);
-    printf("%d\n", scanfReturn);
-    // for (i = -m; i < 0; i += n) {
-    //   printf("%d ", i);
-    // }
+  while (scanf("%d%c", &m, &c) == 2) {
+    if (c == '\n') {
+      if (m == 0) {
+        printf("valid\n");
+        continue;
+      } else {
+        printf("invalid\n");
+        continue;
+      }
+    }
 
-    // printf("0 ");
+    scanf("%d%c", &num, &c);
+    if (c == '\n') {
+      printf("invalid\n");
+      continue;
+    }
 
-    // for (i = m % n ? m % n : n; m >= i; i += n) {
-    //   if (m - i) {
-    //     printf("%d ", i);
-    //     continue;
-    //   }
-    //   printf("%d ", i);
-    // }
-    // printf("\n");
+    n = num - m;
+    isValid = 1;
+    isEnd = 0;
+
+    for (i = m + n + n; 0 > i; i += n) {
+      scanf("%d%c", &num, &c);
+      if (c == '\n') {
+        printf("invalid\n");
+        isValid = 0;
+        isEnd = 1;
+        break;
+      }
+
+      if (num != i) {
+        printf("invalid\n");
+        isValid = 0;
+        break;
+      }
+    }
+
+    scanf("%d%c", &num, &c);
+    if (num != 0) {
+      printf("invalid\n");
+      isValid = 0;
+    }
+
+    for (i = -m % n ? -m % n : n; -m >= i && isValid; i += n) {
+      scanf("%d%c", &num, &c);
+      if (c == '\n' && num != -m) {
+        printf("invalid\n");
+        isValid = 0;
+        isEnd = 1;
+        break;
+      }
+
+      if (num != i) {
+        printf("invalid\n");
+        isValid = 0;
+        break;
+      }
+    }
+
+    if (isValid == 0) {
+      while (isEnd == 0) {
+        scanf("%d%c", &num, &c);
+        if (c == '\n') {
+          isEnd = 1;
+          break;
+        }
+      }
+    }
+
+    if (isValid == 1) {
+      printf("valid\n");
+    }
   }
 
   return 0;
